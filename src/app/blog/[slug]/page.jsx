3a022478c7709010367afa86b4e70920247af07d
Prashant <acharyaprashant227@gmail.com>
2023-12-30 08:23:54 +0545

    Enhanced Ui little more

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -2,20 +2,22 @@ import React from 'react'
 
 const SlugPage = () => {
   return (
-    <div  className='w-full flex-col'>
-      <div>
-  Blog title
+    <div  className='w-full flex-col justify-center flex items-center '>
+      <div className='text-3xl my-3'>
+The change in this world is thretening the future
 </div>
 <div>
 <img className="rounded-t-lg" height={"200px"} width={"200px"} src="https://imageio.forbes.com/specials-images/imageserve/5faad4255239c9448d6c7bcd/Best-Animal-Photos-Contest--Close-Up-Of-baby-monkey/960x0.jpg?format=jpg&width=960" alt="" />
 </div>
-<div>
+<div className='my-3 text-center'>
   <p>Ram Verma</p>
 <p>2013/03/23</p>
 </div>
 
-<div>
-  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus fugit adipisci dolorem accusamus libero sapiente maiores quam quibusdam, distinctio consequatur sit asperiores et quod quis reiciendis, dolore itaque culpa aut voluptatem veritatis odio dignissimos nesciunt explicabo commodi! Sint enim porro, neque consequatur ipsa labore sequi ipsam soluta veritatis, eaque, libero itaque explicabo harum incidunt? Accusamus.
+<div className='px-7 mb-6 text-xl'>
+  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus fugit adipisci dolorem accusamus libero sapiente maiores quam quibusdam, distinctio consequatur sit asperiores et quod quis reiciendis, dolore itaque culpa aut voluptatem veritatis odio dignissimos nesciunt explicabo commodi! Sint enim porro, neque consequatur ipor sit, amet consectetur adipisicing elit. Voluptatibus fugit adipisci dolorem accusamus libero sapiente maiores quam quibusdam, distinctio consequatur sit asperiores et quod quis reiciendis, dolore itaque culpa aut voluptatem veritatis odio dignissimos nesciunt explicabo commodi! Sint enim porro, neque consequatur ipor sit, amet consectetur adipisicing elit. Voluptatibus fugit adipisci dolorem accusamus libero sapiente maiores quam quibusdam, distinctio consequatur sit asperiores et quod quis reiciendis, dolore 
+  <br /><br />
+  itaque culpa aut voluptatem veritatis odio dignissimos nesciunt explicabo commodi! Sint enim porro, neque consequatur ipor sit, amet consectetur adipisicing elit. Voluptatibus fugit adipisci dolorem accusamus libero sapiente maiores quam quibusdam, distinctio consequatur sit asperiores et quod quis reiciendis, dolore itaque culpa aut voluptatem veritatis odio dignissimos nesciunt explicabo commodi! Sint enim porro, neque consequatur ipsa labore sequi ipsam soluta veritatis, eaque, libero itaque explicabo harum incidunt? Accusamus.
 </div>
     </div>
   )
