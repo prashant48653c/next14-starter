03e8d31c6b6f47f508e2da71f02cc2eca82b1d0c
Prashant <acharyaprashant227@gmail.com>
2023-12-31 20:08:59 +0545

    Learned more about next

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,8 +1,19 @@
+import { useSession, signIn, signOut } from "next-auth/react"
 
-const LoginPage = () => {
+export default function Component() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
   return (
-    <div>LoginPage</div>
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
   )
 }
-
-export default LoginPage
